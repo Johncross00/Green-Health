1ff6f3af85219c928d78f49b08d36e31c35b0bcf
johncross00 <johncross.ktm@gmail.com>
2024-12-27 12:00:21 +0000

    try fixes migr

--- app/Http/Controllers/OperateurController.php
+++ app/Http/Controllers/OperateurController.php
@@ -10,28 +10,34 @@ class OperateurController extends Controller
 {
     //
     protected $opera;
-    public function __construct(OperateurService $service){
-        $this->opera=$service;
+    public function __construct(OperateurService $service)
+    {
+        $this->opera = $service;
     }
-    public function create(){
+    public function create()
+    {
         return view('operateurs.create');
     }
-    public function activer(Request $request){
+    public function activer(Request $request)
+    {
         return $this->opera->activer($request->all());
     }
-    public function deactiver(Request $request){
+    public function deactiver(Request $request)
+    {
         return $this->opera->deactiver($request->all());
     }
-    public function operateurs(){
-        $operateurs=$this->opera->operateurs();
+    public function operateurs()
+    {
+        $operateurs = $this->opera->operateurs();
         return view('operateurs.list', compact('operateurs'));
     }
-    public function post_operateur(Request $request){
+    public function post_operateur(Request $request)
+    {
         $request->validate([
-            'localite'=>'string|required',
+            'localite' => 'string|required',
 
         ]);
-        
+
         $user = Auth::user();
 
         if ($user->operateur) {
@@ -39,33 +45,37 @@ class OperateurController extends Controller
         }
         return $this->opera->create($request->all());
     }
-    public function info(){
+    public function info()
+    {
         // $operateurs=$this->opera->operateurs();
         return view('operateurs.info');
     }
-    public function coin_balance(){
-    return view('operateurs.chargement');
+    public function coin_balance()
+    {
+        return view('operateurs.chargement');
     }
-    public function chargement(Request $request){
+    public function chargement(Request $request)
+    {
         $request->validate([
-            'identifiant'=>'required|string',
-            'valeur'=>'string|required',
+            'identifiant' => 'required|string',
+            'valeur' => 'string|required',
         ]);
         return $this->opera->chargement($request->all());
-        
     }
 
-    public function update_operateur_localite(Request $re){
+    public function update_operateur_localite(Request $re)
+    {
         $re->validate([
-            'location'=>'required|string',
+            'location' => 'required|string',
         ]);
         return $this->opera->update($re->all());
     }
-    public function cash(Request $request){
-   $request->validate([
-    'sommes'=>'string|required',
-    'identifiant'=>'string|required',
-   ]);
-   return $this->opera->cash($request->all());
+    public function cash(Request $request)
+    {
+        $request->validate([
+            'sommes' => 'string|required',
+            'identifiant' => 'string|required',
+        ]);
+        return $this->opera->cash($request->all());
     }
 }
